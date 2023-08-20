da25824bd41902b7bf5ebefdd59380bb9a961237
Lukas Kusik <lukas.kusik@gmail.com>
2023-08-20 09:27:09 +0200

    Update user agent string

--- magiogo.py
+++ magiogo.py
@@ -14,7 +14,7 @@ from requests.adapters import HTTPAdapter
 from client import *
 import datetime
 
-UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0'
+UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
 
 
 class MagioGoException(Exception):
